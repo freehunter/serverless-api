749bd7896e6578514ed39e11ecb67b17a386fb29
Mark Hunt <thefreehunter@gmail.com>
2020-01-31 03:38:54 +0000

    added new db field to create api

--- create.js
+++ create.js
@@ -10,7 +10,8 @@ export async function main(event, context) {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
       content: data.content,
-      attachment: data.attachment,
+      happy: data.happy,
+      alcohol: data.alcohol,
       createdAt: Date.now()
     }
   };
